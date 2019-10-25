778aa4877506546df3b2fc0cb8aceceddfff053c
vostebush <vostowoko@yahoo.com>
2019-10-25 16:01:51 +0300

    Error fixing

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -12,7 +12,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,9 +6,9 @@ import 'rxjs/add/operator/map';
   export class ProductService {
     private _albumUrl = "../assets/album.json";
 
-    constructor(private _http:Http) { }
+    constructor(private _http:Http) {}
 
-    getAlbum(id:Number) {
+    getAlbum(id: number) {
       return this._http.get(this._albumUrl).map((response) => response.json());
     }   
 
